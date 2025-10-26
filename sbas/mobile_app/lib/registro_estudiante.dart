6a17e5325dae69979f5c70d90579267da15cc8e8
Hyos332 <luis.hoyos1@alumnos.uneatlantico.es>
2025-10-26 20:10:22 +0100

    feat: enhance Attendance and Beacon models with relation names and improve registration screen structure

--- sbas/mobile_app/lib/registro_estudiante.dart
+++ sbas/mobile_app/lib/registro_estudiante.dart
@@ -5,6 +5,8 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class RegistroEstudiante extends StatefulWidget {
+  const RegistroEstudiante({super.key});
+
   @override
   _RegistroEstudianteState createState() => _RegistroEstudianteState();
 }
